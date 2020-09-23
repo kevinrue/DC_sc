cfac95187a7c24b1c6dc42836407df4f81c1994e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-09-23 14:47:36 +0100

    export count, logcount, metadata

--- 12_sce_infected/99_export.R
+++ 12_sce_infected/99_export.R
@@ -1,7 +1,9 @@
+sce <- readRDS("12_sce_infected/rds/sce.norm.rds")
 
-write.table(x = assay(sce, "counts"), file = "~/Desktop/DC/count_matrix.txt", quote = FALSE, sep = "\t")
+write.table(x = assay(sce, "counts"), file = "12_sce_infected/export/count_matrix.txt", quote = FALSE, sep = "\t")
+write.table(x = assay(sce, "logcounts"), file = "12_sce_infected/export/logcount_matrix.txt", quote = FALSE, sep = "\t")
 
-write.table(colData(sce)[, c("Sample", "Infection", "Status", "Time", "Lane", "Plate",  "Well")], "~/Desktop/DC/cell_metadata.txt", quote = FALSE, sep = "\t")
+write.table(colData(sce)[, c("Sample", "Infection", "Status", "Time", "Lane", "Plate",  "Well")], "12_sce_infected/export/cell_metadata.txt", quote = FALSE, sep = "\t")
 
-write.table(rowData(sce)[, c("source", "type", "score", "phase", "gene_id", "gene_version",  "gene_name", "gene_source", "gene_biotype", "havana_gene", "havana_gene_version",  "transcript_id")], "~/Desktop/DC/gene_metadata.txt", quote = FALSE, sep = "\t")
+write.table(rowData(sce)[, c("source", "type", "score", "phase", "gene_id", "gene_version",  "gene_name", "gene_source", "gene_biotype", "havana_gene", "havana_gene_version",  "transcript_id")], "12_sce_infected/export/gene_metadata.txt", quote = FALSE, sep = "\t")
 
